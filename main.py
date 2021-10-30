53d0fac496840e5a1c1d451af976854c6e5f392f
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-30 23:31:08 +0300

    create a list from the invited names

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+names_list =[]
+with open("Input/Names/invited_names.txt") as names:
+    contents = names.read()
+    list_names = list(contents.split("\n"))
+    print(list_names)
+
+
